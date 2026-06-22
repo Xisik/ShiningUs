b2506e232ddd1f665ada2ba147f78a34a91d514d
kaede982006 <kaede982006@gmail.com>
2026-06-23 05:59:04 +0900

    Fix Google Analytics deployment

--- assets/js/01_analytics.js
+++ assets/js/01_analytics.js
@@ -1,11 +1,16 @@
 const GA_TRACKING_ID = 'REPLACE_ME_GA_ID';
+const GA_PLACEHOLDER = 'REPLACE_ME_' + 'GA_ID';
 
-const gtagScript = document.createElement('script');
-gtagScript.async = true;
-gtagScript.src = `https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`;
-document.head.appendChild(gtagScript);
+if (!GA_TRACKING_ID || GA_TRACKING_ID === GA_PLACEHOLDER) {
+  console.warn('Google Analytics measurement ID is not configured.');
+} else {
+  const gtagScript = document.createElement('script');
+  gtagScript.async = true;
+  gtagScript.src = `https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`;
+  document.head.appendChild(gtagScript);
 
-window.dataLayer = window.dataLayer || [];
-function gtag(){ window.dataLayer.push(arguments); }
-gtag('js', new Date());
-gtag('config', GA_TRACKING_ID);
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){ window.dataLayer.push(arguments); }
+  gtag('js', new Date());
+  gtag('config', GA_TRACKING_ID);
+}
